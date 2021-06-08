9dad1c75085c81e84e45c3c8fe631b59393912df
Peter <peter.zmilczak@gmail.com>
2021-06-08 22:03:08 +0200

    added examples

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Example from "./Example";
+import {withPrefix} from "../../.cache/commonjs/gatsby-browser-entry";
 
 class Examples extends Component {
 
     render() {
         return (
             <div style={{display: "flex", justifyContent: "center", marginTop: 50}}>
-                <Example src={"../../example1.png"} url={"https://github.com/marwin1991"}
+                <Example src={withPrefix("../../example1.png")} url={"https://github.com/marwin1991"}
                          text={"Here is my profile on the GitHub " +
                          "service, where I have add icons generated with this site. On a daily basis " +
                          "I try to make World a better place using Java 🌎 \u2728 🤣"}/>
