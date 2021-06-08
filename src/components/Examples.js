bd0a7f778ab2ada01f973e1335a885f9b379ca5c
Peter <peter.zmilczak@gmail.com>
2021-06-08 21:54:55 +0200

    added examples

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import example1 from "../images/example1.png"
 import Example from "./Example";
 
 class Examples extends Component {
@@ -7,7 +6,7 @@ class Examples extends Component {
     render() {
         return (
             <div style={{display: "flex", justifyContent: "center", marginTop: 50}}>
-                <Example src={example1} url={"https://github.com/marwin1991"}
+                <Example src={"../../example1.png"} url={"https://github.com/marwin1991"}
                          text={"Here is my profile on the GitHub " +
                          "service, where I have add icons generated with this site. On a daily basis " +
                          "I try to make World a better place using Java 🌎 \u2728 🤣"}/>
--- src/pages/header.js
+++ src/pages/header.js
@@ -6,13 +6,18 @@ import Typography from '@material-ui/core/Typography';
 
 
 export default function ElevateAppBar(props) {
-  return (
-    <React.Fragment>
-        <AppBar style={{background: "#ffffff"}}>
-          <Toolbar>
-            <Typography variant="h6" style={{color:"#26292d"}}>Profile Technology Icons</Typography>
-          </Toolbar>
-        </AppBar>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <AppBar style={{background: "#ffffff"}}>
+                <Toolbar>
+                    <Typography variant="h6" style={{color: "#26292d"}}>Profile Technology Icons</Typography>
+                    <div style={{marginLeft: 15, marginRight: 15}}>
+                        <iframe
+                            src="https://ghbtns.com/github-btn.html?user=marwin1991&repo=profile-technology-icons&type=star&count=true&size=large"
+                            frameBorder="0" scrolling="0" width="170" height="30" title="GitHub"/>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
 }
